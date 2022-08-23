0ece54da4c26a42028511b5e847dfcace4c6fd37
Tom Hayes <hello@tomhay.es>
2022-08-23 18:29:17 +0100

    customisation for containers

--- wp-content/themes/posit/tailwind.config.cjs
+++ wp-content/themes/posit/tailwind.config.cjs
@@ -1,6 +1,10 @@
 module.exports = {
   content: ["./index.php", "./app/**/*.php", "./resources/**/*.{php,vue,js}"],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+    },
     extend: {
       colors: {},
     },
