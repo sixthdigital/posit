9a8971900a6998f57d4b2f586e18d8fe70fe88c2
Tom Hayes <hello@tomhay.es>
2022-08-23 18:32:36 +0100

    footer basics

--- wp-content/themes/posit/resources/views/sections/footer.blade.php
+++ wp-content/themes/posit/resources/views/sections/footer.blade.php
@@ -1,3 +1,7 @@
-<footer class="content-info">
-  @php(dynamic_sidebar('sidebar-footer'))
+<footer class="content-info container">
+
+
+  <div class="sub-footer">
+    <span>Site by <a href="https://www.sixthdigital.co.uk">Sixth Digital</a></span>
+  </div>
 </footer>
